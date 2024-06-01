63050205b407cd6daf0de1333435323c9d5bcf02
gauresh123 <waranggauresh@gmail.com>
2024-06-01 13:14:59 +0530

    logic part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,86 @@
-import React from 'react';
-import { Container, TextField, Button, Box } from '@mui/material';
+import React, { useState } from "react";
+import {
+  Container,
+  TextField,
+  Button,
+  Box,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 function InputAndButton() {
+  const [inputVal, setInputVal] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleChange = (e) => {
+    setInputVal(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setData([...data, inputVal]);
+  };
+
   return (
-    <Container maxWidth="sm" sx={{ textAlign: 'center', marginTop: '50px' , backgroundColor: 'purple', padding: '20px' }}>
-      <Box display="flex" alignItems="center" justifyContent="center" flexDirection="row">
-        <TextField label="Enter todo Inputs" variant="outlined" fullWidth 
-          sx={{ marginRight: '10px',
-                borderRadius:'100px',
-                backgroundColor: 'white' }} />
-        <Button variant="contained" color="primary"
-         sx={{borderRadius:'500px'}}>
+    <Container
+      maxWidth="sm"
+      sx={{
+        textAlign: "center",
+        marginTop: "50px",
+        backgroundColor: "purple",
+        padding: "20px",
+      }}
+    >
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="row"
+      >
+        <TextField
+          placeholder="Enter todo Inputs"
+          variant="outlined"
+          fullWidth
+          value={inputVal}
+          onChange={handleChange}
+          sx={{
+            marginRight: "10px",
+            borderRadius: "100px",
+            backgroundColor: "white",
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                border: "none",
+              },
+            },
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ borderRadius: "500px" }}
+          onClick={handleAdd}
+        >
           +
         </Button>
       </Box>
+
+      <Stack>
+        {data?.map((val, i) => {
+          return (
+            <Box
+              p={1}
+              border={1}
+              sx={{ backgroundColor: "white" }}
+              borderColor={"silver"}
+              m={1}
+              borderRadius={3}
+            >
+              <Typography color={"black"} textAlign={"left"}>
+                {val}
+              </Typography>
+            </Box>
+          );
+        })}
+      </Stack>
     </Container>
   );
 }
