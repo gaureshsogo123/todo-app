8fa34acd15d2a03042fab9af0df3170617d232cf
Durgaravi-04 <durgaravi028@gmail.com>
2024-06-01 12:18:12 +0530

    added input text and button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from 'react';
+import { Container, TextField, Button, Box } from '@mui/material';
 
-function App() {
-  return <div>app</div>;
+function InputAndButton() {
+  return (
+    <Container maxWidth="sm" sx={{ textAlign: 'center', marginTop: '50px' , backgroundColor: 'purple', padding: '20px' }}>
+      <Box display="flex" alignItems="center" justifyContent="center" flexDirection="row">
+        <TextField label="Enter todo Inputs" variant="outlined" fullWidth 
+          sx={{ marginRight: '10px',
+                borderRadius:'100px',
+                backgroundColor: 'white' }} />
+        <Button variant="contained" color="primary"
+         sx={{borderRadius:'500px'}}>
+          +
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
-export default App;
+export default InputAndButton;
